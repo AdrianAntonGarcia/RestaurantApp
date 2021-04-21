fc68a74b7e12092a04016c6146d20de4fe0c804b
adriton <adrtler@gmail.com>
2021-04-21 13:16:58 +0200

    Agregando react navigator

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import {Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import DetallePlatillo from './views/DetallePlatillo';
+import FormularioPlatillo from './views/FormularioPlatillo';
+import Menu from './views/Menu';
+import NuevaOrden from './views/NuevaOrden';
+import ProgresoPedido from './views/ProgresoPedido';
+import ResumenPedido from './views/ResumenPedido';
+
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <>
-      <Text>Hola</Text>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="NuevaOrden"
+            component={NuevaOrden}
+            options={{title: 'Nueva Orden'}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 };
---
+++ views/DetallePlatillo.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const DetallePlatillo = () => {
+  return <Text>Detalle Platillo</Text>;
+};
+
+export default DetallePlatillo;

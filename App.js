c8d3b0c41254d563e4798bde4863c1d92e814ae8
adriton <adrtler@gmail.com>
2021-04-21 16:29:38 +0200

    Agregando las diferentes vistas

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import DetallePlatillo from './views/DetallePlatillo';
@@ -16,11 +15,44 @@ const App = () => {
   return (
     <>
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: '#FFDA00',
+            },
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}>
           <Stack.Screen
             name="NuevaOrden"
             component={NuevaOrden}
             options={{title: 'Nueva Orden'}}
+          />
+          <Stack.Screen
+            name="Menu"
+            component={Menu}
+            options={{title: 'Nuesto Menú'}}
+          />
+          <Stack.Screen
+            name="DetallePlatillo"
+            component={DetallePlatillo}
+            options={{title: 'Detalle Platillo'}}
+          />
+          <Stack.Screen
+            name="FormularioPlatillo"
+            component={FormularioPlatillo}
+            options={{title: 'Ordenar Platillo'}}
+          />
+          <Stack.Screen
+            name="ResumenPedido"
+            component={ResumenPedido}
+            options={{title: 'Resumen Pedido'}}
+          />
+          <Stack.Screen
+            name="ProgresoPedido"
+            component={ProgresoPedido}
+            options={{title: 'Progreso de pedido'}}
           />
         </Stack.Navigator>
       </NavigationContainer>
